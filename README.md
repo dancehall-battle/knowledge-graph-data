15c205889613a3cda6e0e356aa5523c98cce3ec1
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-07-21 18:50:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Download data for knowledge graph
+
+## Requirements
+- Node js
+- yarn
+
+## Install
+Install dependencies via `yarn install`.
+
+## Usage
+
+Execute `node downloaddata.js [sheet-id]` 
+to download the Google Sheets with id `sheet-id` 
+as CSV files in the folder `files`.
+
+## License
+
+MIT &copy; Dancehallbattle.org
---
+++ downloaddata.js
@@ -0,0 +1,58 @@
+const GoogleSpreadsheet = require('google-spreadsheet');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const fs = require('fs-extra');
+
+if (process.argv.length !== 3) {
+  console.error('Please provide the id of the Google Sheets.');
+  process.exit(1);
+}
+
+const doc = new GoogleSpreadsheet(process.argv[2]);
+const wantedSheetTitles = ['events', 'organizers', 'battles', 'battle_winner', 'winners'];
+
+// Make sure the directory 'files' exists.
+fs.ensureDirSync('./files');
+
+doc.getInfo((err, info) => {
+  info.worksheets.forEach(sheet => {
+    if (wantedSheetTitles.indexOf(sheet.title) !== -1) {
+      downloadSheetAsCSV(sheet);
+    }
+  });
+});
+
+async function downloadSheetAsCSV(sheet) {
+  return new Promise(async (resolve, reject) => {
+    const header = await getHeaders(sheet);
+
+    const csvWriter = createCsvWriter({
+      path: `./files/${sheet.title}.csv`,
+      header
+    });
+
+    sheet.getRows(async (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        await csvWriter.writeRecords(rows);
+        resolve();
+      }
+    });
+  });
+}
+
+function getHeaders(sheet) {
+  return new Promise((resolve, reject) => {
+    sheet.getRows({offset: 0, limit: 1}, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        const row = rows[0];
+        resolve(Object.keys(row)
+          .filter(key => (!key.startsWith('_') && key !== 'save' && key !== 'del' ))
+          .map(header => { return {id: header, title: header}})
+        );
+      }
+    });
+  });
+}
